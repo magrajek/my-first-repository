385f0603d3317ea09398893d829375ae510a7581
Grajek Marek <rb21690G@raiffeisen.pl>
2016-07-26 11:32:21 +0200

    Initialised project

---
+++ src/main/java/pl/raiffeisensolutions/HelloWorld.java
@@ -0,0 +1,14 @@
+package pl.raiffeisensolutions;
+
+/**
+ * Created by rb21690 on 2016-07-26.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+
+
+}
